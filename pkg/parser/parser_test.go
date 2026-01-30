1a17a4333bdd19963f2ee78c56544bc6aba116ba
Naman Agarwal <naman012ag@gmail.com>
2026-01-30 18:50:47 +0530

    Auto-backup before edit

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -171,12 +171,14 @@ func TestCSVParserCustomDelimiter(t *testing.T) {
 	cfg := DefaultConfig()
 	cfg.Delimiter = ';'
 	p := NewCSVParser(cfg)
-	out := make(chan *model.Event, 10)
+	out := make(chan *model.Event, 100)
 
-	err := p.Parse(context.Background(), strings.NewReader(input), out)
-	if err != nil {
-		t.Fatalf("Parse error: %v", err)
-	}
+	go func() {
+		defer close(out)
+		if err := p.Parse(context.Background(), strings.NewReader(input), out); err != nil {
+			t.Errorf("Parse error: %v", err)
+		}
+	}()
 
 	events := drainChannel(out)
 	if len(events) != 1 {
