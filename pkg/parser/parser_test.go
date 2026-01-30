6d657b6280c0dd5c6607644acdf5120fe1319723
Naman Agarwal <naman012ag@gmail.com>
2026-01-30 18:51:27 +0530

    Auto-backup before edit

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -306,12 +306,14 @@ func TestJSONLParserBasic(t *testing.T) {
 `
 
 	p := NewJSONLParser(DefaultConfig())
-	out := make(chan *model.Event, 10)
+	out := make(chan *model.Event, 100)
 
-	err := p.Parse(context.Background(), strings.NewReader(input), out)
-	if err != nil {
-		t.Fatalf("JSONL Parse error: %v", err)
-	}
+	go func() {
+		defer close(out)
+		if err := p.Parse(context.Background(), strings.NewReader(input), out); err != nil {
+			t.Errorf("JSONL Parse error: %v", err)
+		}
+	}()
 
 	events := drainChannel(out)
 	if len(events) != 2 {
